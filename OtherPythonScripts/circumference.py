a1fdb9d4d662590adce22f62708f3c99facb5098
Dr. Oliver Demuth <105116778+OliverDemuth@users.noreply.github.com>
2026-02-23 12:18:24 +0000

    Update circumference.py

--- OtherPythonScripts/circumference.py
+++ OtherPythonScripts/circumference.py
@@ -7,7 +7,7 @@
 #	'circumference()' in the Python command line in Maya. 
 #
 #	Written by Oliver Demuth 
-#	Last updated 10.10.2025 - Oliver Demuth 
+#	Last updated 23.02.2026 - Oliver Demuth 
 #
 #
 #	IMPORTANT notes:
@@ -42,72 +42,57 @@ def circumference():
 
 	# get vertices from mesh
 	
-	vertices = fnMesh.getPoints(4) # get world position of all vertices (om.MSpace.kWorld = 4)
-
-	# get coordinates and transform them into matrices
-	
-	vtcPos = np.array(vertices)
-	vtxArr = np.stack([np.eye(4)] * vtcPos.shape[0], axis = 0)
-	vtxArr[:,3,:] = vtcPos # append coordinates to rotation matrix array
-
-	# get centre position of mesh (i.e., centre of bounding box)
-
-	fnMeshMat = np.eye(4)
-	fnMeshMat[3,:] = np.array(fnMesh.boundingBox.center)
-	transMat = np.array(dag.inclusiveMatrix()).reshape(4,4) # mesh transofrmation matrix
-
-	meanPos = om.MPoint(np.dot(fnMeshMat,transMat)[3,:]) # bounding box centre poosition
+	vertices = np.array(fnMesh.getPoints(4)) # get world position of all vertices (om.MSpace.kWorld = 4)
+	meanPos = vertices.mean(axis = 0)
 
 	# calculate vectors from meanPoint to any two vertices to define plane
 
-	ADir = meanPos - vertices[0]
-	BDir = meanPos - vertices[1]
+	x = vertices[0] - meanPos
+	x = x / np.linalg.norm(x) # normalised
+	z = np.cross(vertices[1] - meanPos, x)
+	z = z / np.linalg.norm(z) # normalised
+	y = np.cross(z, x)
 
-	# calculate cut plane direction
-
-	uDir = ADir.normal() 
-	wDir = (BDir.normal() ^ uDir).normal() # cross product to get z axis
-	vDir = (wDir ^ uDir).normal() # cross product to get y axis 
-
-	# get transformation matrix for cutting plane
-
-	rotMat = np.array([[uDir.x, uDir.y, uDir.z, 0],
-			   [vDir.x, vDir.y, vDir.z, 0],
-			   [wDir.x, wDir.y, wDir.z, 0],
-			   [meanPos[0],meanPos[1],meanPos[2],1]]) # centre position around midpoint
-
+	# assemble transformation matrix (i.e., inverse of slice coordinate system)
+	
+	transMat = np.eye(4)
+	transMat[0:3,0] = x
+	transMat[0:3,1] = y
+	transMat[0:3,2] = z
+	transMat[3,0:3] = -meanPos @ transMat[0:3,0:3]
+	
 	# calculate new coordinates and extract them
 
-	uvCoords = np.dot(vtxArr,np.linalg.inv(rotMat))[:,3,0:2] # only need first two values (i.e., u and v coordinates)
+	uvCoords = (vertices @ transMat)[:,0:2] # only need first two values (i.e., u and v coordinates)
 
 	# calculate convex hull
 
-	hullPoints = MC_convex_hull(uvCoords.tolist())
+	hullPoints = MC_convex_hull(uvCoords)
 
 	# calculate perimeter/circumference of convex hull
 
-	return float(sum(np.sqrt(sum(np.diff(hullPoints,axis=0).T**2))))
+	diff = hullPoints[1:] - hullPoints[:-1]
+	
+	return np.sqrt((diff * diff).sum(axis = 1)).sum()
 
 
 def MC_convex_hull(points):
 
 	# Input variables:
-	#	points = points on mesh slice (2D coordinates U and V) as a Python list
+	#	points = points on mesh slice (2D coordinates U and V) as a np.array
 	# ======================================== #
+
+	if points.shape[0] <= 1: 
+		return points.copy()
 
 	# sort points based on u and v coordinates
 	
-	points.sort(key=lambda point:[point[0],point[1]]) 
-
-	# if equal to or less than 1 point return points list
-	
-	if len(points) <= 1:
-		return np.array(points)
+	idx = np.lexsort((points[:,1], points[:,0])) 
+	points = points[idx]
 
 	# get lower hull 
 
 	lower = []
-
 	for p in points:
 		while len(lower) >= 2 and ccw(lower[-2], lower[-1], p) <= 0:
 			lower.pop()
@@ -116,8 +101,7 @@ def MC_convex_hull(points):
 	# get upper hull
 
 	upper = []
-
-	for p in reversed(points):
+	for p in points[::-1]:
 		while len(upper) >= 2 and ccw(upper[-2], upper[-1], p) <= 0:
 			upper.pop()
 		upper.append(p)
@@ -126,7 +110,7 @@ def MC_convex_hull(points):
 	# Last point of lower list is omitted because it is repeated at the beginning of the upper list.
 	# Start point is duplicated for circumference calculation.
 
-	return np.array(lower[:-1] + upper)
+	return np.vstack((lower[:-1],upper))
 
 
 def ccw(A, B, C):
@@ -137,13 +121,5 @@ def ccw(A, B, C):
 	#	C = third corner of triangle (2D coordinates U and V)
 	# ======================================== #
 
-	# Return 1 if counter clockwise, -1 if clock wise, 0 if collinear
-
-	area = int(((B[0] - A[0]) * (C[1] - A[1]) - (B[1] - A[1]) * (C[0] - A[0]))*10**10)/10.0**10 # prevents floating point errors
-		
-	if area > 0:
-		return 1
-	elif area < 0:
-		return -1
-	else:
-		return 0
+	return (B[0] - A[0]) * (C[1] - A[1]) - (B[1] - A[1]) * (C[0] - A[0])
+	
